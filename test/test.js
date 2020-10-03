b0d7bbdc059f7c16ff1211d6df056cbfa4ef4d63
Jim Speir <jimbo80982@gmail.com>
2020-10-03 01:37:02 +0100

    Adding basic tests

---
+++ test/test.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+var request = require('supertest');
+
+describe('Make sure it responds', () => {
+  it('Should respond on port 80', () => {
+    request("localhost:80")
+      .get("/")
+      .expect(200);
+  }
+});
+
